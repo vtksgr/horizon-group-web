9ba1ae1cb640ecd94c0d587949577ad1c9ff1de8
Sagar Pariyar <vtksgr@gmail.com>
2026-04-14 22:02:41 +0900

    Make Railway startup more resilient

--- server/src/server.js
+++ server/src/server.js
@@ -4,15 +4,43 @@ import app from "./app.js";
 import logger from "./utils/logger.js";
 
 const PORT = process.env.PORT || 5000;
+const HOST = "0.0.0.0";
+const DB_CONNECT_RETRY_DELAY_MS = 5000;
 
-async function startServer() {
+async function connectDatabaseWithRetry() {
   try {
     await prisma.$connect();
     logger.info("database_connected");
-
-    app.listen(PORT, () => {
-      logger.info("server_started", { port: Number(PORT) });
+  } catch (error) {
+    logger.error("database_connect_failed", {
+      message: error.message,
+      retryInMs: DB_CONNECT_RETRY_DELAY_MS,
     });
+
+    setTimeout(() => {
+      void connectDatabaseWithRetry();
+    }, DB_CONNECT_RETRY_DELAY_MS);
+  }
+}
+
+async function startServer() {
+  try {
+    const server = app.listen(PORT, HOST, () => {
+      logger.info("server_started", {
+        port: Number(PORT),
+        host: HOST,
+      });
+    });
+
+    server.on("error", (error) => {
+      logger.error("server_runtime_error", {
+        message: error.message,
+        stack: process.env.NODE_ENV === "development" ? error.stack : undefined,
+      });
+      process.exit(1);
+    });
+
+    void connectDatabaseWithRetry();
   } catch (error) {
     logger.error("server_start_failed", {
       message: error.message,
